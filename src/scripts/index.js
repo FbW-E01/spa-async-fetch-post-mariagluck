da6c5ccb34217c62269e44ee41ab812fa7932a59
mariagluck <mariamart.berlin@gmail.com>
2021-06-29 14:38:45 +0200

    kind of done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,4 +4,70 @@ import "babel-polyfill";
 
 
 // \/ All of your javascript should go here \/
+const form = document.querySelector("#submit-form");
+const name = document.querySelector("#name");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const message  = document.querySelector("#message");
+const checkbox = document.querySelector("#checkbox");
+
+// this is one way without async await
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    // console.log("Form was submitted!");
+    const user = {
+        name: name.value,
+        email: email.value,
+        password: password.value,
+        message: message.value,
+        checkbox: checkbox.value
+    };
+
+    // console.log(user);
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+        body: JSON.stringify(user)
+    })
+       .then(response => {
+           console.log(response.ok);
+           return response.json()   
+       })
+       .then(result => console.log(user))
+       .then(alert("Thank you for submitting your details."))
+       
+});
+
+
+
+///this is with async await 
+
+// const url = "htps://jsonplaceholder.typicode.com/posts";
+
+// form.addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     const user = {
+//         name: name.value,
+//         email: email.value,
+//         password: password.value,
+//         message: message.value,
+//         checkbox: checkbox.value
+//     };
+//     console.log(user);
+//     getUsers(user);
+//     alert("Thank you for submitting your details.");
+// });
+
+
+// const getUsers = async () => {
+//     try {
+//         const response = await fetch(url, {
+//             method: "POST",
+//             body: JSON.stringify(user),
+//         });
+//         return response;
+//     } catch {
+//         console.warn("Error");
+//     }
+// }
 
